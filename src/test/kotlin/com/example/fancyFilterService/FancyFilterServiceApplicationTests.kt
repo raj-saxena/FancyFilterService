ecf91347f31dbe917b9b777e6379d81cd80f821a
Raj <the.raj.saxena@gmail.com>
2019-04-27 12:48:02 +0200

    Replaced JUnit4 with JUnit5

--- src/test/kotlin/com/example/fancyFilterService/FancyFilterServiceApplicationTests.kt
+++ src/test/kotlin/com/example/fancyFilterService/FancyFilterServiceApplicationTests.kt
@@ -1,16 +1,16 @@
 package com.example.fancyFilterService
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForObject
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FancyFilterServiceApplicationTests {
     @Autowired
