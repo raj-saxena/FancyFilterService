3668e27829de440ebe9815c11a64431004ff9992
Raj <the.raj.saxena@gmail.com>
2019-05-04 14:48:18 +0200

    [#2] Refactor DataImporter to use UserService

--- src/main/kotlin/com/example/fancyFilterService/services/UserService.kt
+++ src/main/kotlin/com/example/fancyFilterService/services/UserService.kt
@@ -1,5 +1,6 @@
 package com.example.fancyFilterService.services
 
+import com.example.fancyFilterService.dtos.User
 import com.example.fancyFilterService.dtos.Users
 import com.example.fancyFilterService.repositories.UserRepository
 import org.springframework.stereotype.Service
@@ -7,4 +8,6 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(val userRepository: UserRepository) {
     fun getUsers(): Users = Users(userRepository.getUsers())
+    fun getUserCount(): Int = TODO()
+    fun save(users: List<User>): Unit = TODO()
 }
