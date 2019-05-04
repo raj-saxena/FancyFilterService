68b2d6149a400d791d710cb5eee8ce2508b50a8f
Raj <the.raj.saxena@gmail.com>
2019-05-04 14:33:17 +0200

    [#2] Return all users

---
+++ src/main/kotlin/com/example/fancyFilterService/controllers/UserController.kt
@@ -0,0 +1,13 @@
+package com.example.fancyFilterService.controllers
+
+import com.example.fancyFilterService.services.UserService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController(val userService: UserService) {
+
+    @GetMapping("/api/users")
+    fun getUsers() = userService.getUsers()
+
+}
---
+++ src/main/kotlin/com/example/fancyFilterService/services/UserService.kt
@@ -0,0 +1,10 @@
+package com.example.fancyFilterService.services
+
+import com.example.fancyFilterService.dtos.Users
+import org.springframework.stereotype.Service
+
+@Service
+class UserService {
+    fun getUsers(): Users = TODO()
+
+}
