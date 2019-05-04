153bd9440576ced400adc496e87860d379f0e56a
Raj <the.raj.saxena@gmail.com>
2019-05-04 13:14:58 +0200

    [1] Added log

--- src/main/kotlin/com/example/fancyFilterService/services/DataImporterCommandLineRunner.kt
+++ src/main/kotlin/com/example/fancyFilterService/services/DataImporterCommandLineRunner.kt
@@ -1,6 +1,7 @@
 package com.example.fancyFilterService.services
 
 import com.example.fancyFilterService.repositories.UserRepository
+import org.apache.logging.log4j.LogManager
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Service
 
@@ -9,10 +10,13 @@ class DataImporterCommandLineRunner(
     val userRepository: UserRepository,
     val jsonImporterService: JsonImporterService
 ) : CommandLineRunner {
+    val log = LogManager.getLogger(javaClass)
+
     override fun run(vararg args: String?) {
         if (userRepository.getUserCount() == 0) {
             val users = jsonImporterService.loadUsers()
             userRepository.save(users.users)
+            log.info("Loaded ${users.users.size} users.")
         }
     }
 }
